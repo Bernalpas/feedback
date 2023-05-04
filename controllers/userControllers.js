6fb879eae845e16070eae8afd6e3efd8a5007fea
Berni <bernalpas@gmail.com>
2023-05-04 15:15:35 -0300

    80% app terminada, tareas realizar el front

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,13 +1,14 @@
 const { validationResult } = require('express-validator') 
 const Usuario = require('../models/userModel');
 const bcrypt = require('bcrypt');
+const { generarJsonWebToken } = require('../middlewares/generarToken')
 
+
+//podemos crear la función de JWT
 
 const paginaPrincipal = (req, res) => { 
     //console.log(req);
-    res.status(200).json({
-        mensaje: "Código 200 - Todo OK!"
-    })
+    res.status(200).render('home');
 }
 
 const paginaError = (req, res) => { 
@@ -15,6 +16,13 @@ const paginaError = (req, res) => {
     res.status(500).send(`<h1>Todo mal!!</h1>`)
 }
 
+const paginaLogin = (req, res) => {
+    res.status(500).send(`<h1>Página para el Login</h1>`)
+}
+
+const paginaRegistro = (req, res) => {
+    res.status(200).render('registro');
+}
 
 const registrarUsuario = async (req, res) => {
     
@@ -43,7 +51,7 @@ const registrarUsuario = async (req, res) => {
         }
 
     //4. si no Existe, creamos un nuevo usuario
-    let nuevoUsuario = new Usuario(req.body);
+    const nuevoUsuario = new Usuario(req.body);
 
     console.log(`3. Nuevo Usuario a guardar: ${nuevoUsuario}`);
 
@@ -62,8 +70,14 @@ const registrarUsuario = async (req, res) => {
     //7. Insertamos en la Database el nuevo usuario
     await nuevoUsuario.save();
 
+    //8. Asignamos el token al usuario
+    let token = await generarJsonWebToken(nuevoUsuario);
+
+    //9. Imprimimos el token del nuevo usuario
+    console.log(token);
+
     //6. Respondemos a la petición del cliente si todo va bien
-    res.status(200).end('Tus datos fueron recibidos y guardados en la DB')
+    res.status(200).render('recibido')
 
     } catch (error) {
         console.log(error);
@@ -71,8 +85,52 @@ const registrarUsuario = async (req, res) => {
             mensaje: 'Nuestros mejores Deps están trabajando para solucionar el Problem'
         })
     }
+}
 
 
+const loginUsuario = async (req, res) => {
+
+    const { email, password } = req.body;
+
+    console.log(`1. Login: ${email} - ${password}`);
+
+    //1. Buscamos el email en la database
+    try {
+        let usuarioExiste = await Usuario.findOne({email}) 
+        console.log(`2. Existe: ${usuarioExiste.email}`);
+        
+        //2. Si no existe le pedimos que se registre
+        if(!usuarioExiste){
+            return res.status(400).json({
+                errores: 'El email NO está registrado - Favor Registrarse'
+            })
+        }
+
+        //3. Vemos el passwor del usuario
+        console.log(`3. Encriptación: ${usuarioExiste.password}`);
+
+        //4. Si existe, validamos su password
+        const validarPassword = bcrypt.compareSync(password, usuarioExiste.password);
+
+        //5. Imprimimos la validación del password
+        console.log(`4. Validado: ${validarPassword}`);
+
+
+        if(validarPassword){
+            return res.status(400).json({
+                Administracion: 'Bienvenido Administrador'
+            })
+        }else{
+            return res.status(400).json({
+                Error: 'Password incorrecto'
+            })
+        }
+
+    } catch (error) {
+        return res.status(400).json({
+            Error: 'Email o Password incorrectos'
+        })
+    }
 }
 
 
@@ -98,7 +156,10 @@ const paginaPrueba = (req, res) => {
 
 module.exports = {
     paginaPrincipal,
-    paginaError,
+    paginaLogin,
+    paginaRegistro,
     registrarUsuario,
-    paginaPrueba
+    loginUsuario,
+    paginaPrueba,
+    paginaError
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,27 +4,37 @@ const router = express.Router();
 const { check, body } = require('express-validator');
 const {
     paginaPrincipal,
-    paginaError,
+    paginaLogin,
+    paginaRegistro,
     registrarUsuario,
-    paginaPrueba
+    loginUsuario,
+    paginaPrueba,
+    paginaError,
 } = require('../controllers/userControllers')
 
 router.get('/', paginaPrincipal);
-
-router.get('/error', paginaError);
+router.get('/login', paginaLogin);
+router.get('/registrar', paginaRegistro);
 
 router.post('/',  
 [
-    check('nombre').isLength({min:4}),
+    check('nombre').isLength({ min:4 }),
     check('email').isEmail(),
-    check('password').isLength({min:5}),
+    check('password').isLength({ min:5 }),
 ], registrarUsuario);
 
+router.post('/login', 
+[
+    check('email').isEmail(),
+    check('password').isLength({ min:5 })
+], loginUsuario);
+
 router.post('/body', 
-    body('nombre').isLength({ min: 4 }),
-    body('email').isEmail(),
-    body('password').isLength({ min: 5 })
+body('nombre').isLength({ min: 4 }),
+body('email').isEmail(),
+body('password').isLength({ min: 5 })
 , paginaPrueba);
 
+router.get('/error', paginaError);
 
 module.exports = router;
